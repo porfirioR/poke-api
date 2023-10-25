d5729f50db30091c8e8bbb3c09ae51dd838235a4
porfirioR <porfirioperezgon@gmail.com>
2023-10-25 14:43:17 -0300

    first steps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,60 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      // 'sm': '640px',
+      // 'md': '768px',
+      // 'lg': '1024px',
+      // 'xl': '1280px',
+      // '2xl': '1536px',
     },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-      'midnight': '#121063',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
+      slate: colors.slate,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      white: colors.white,
+      pink: colors.pink,
+      rose: colors.rose,
+      black: colors.black,
+      midnight: '#121063',
+      metal: '#565584',
+      tahiti: '#3ab7bf',
+      silver: colors.silver,
       'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
+      bermuda: '#78dcca'
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      // sans: ['Graphik', 'sans-serif'],
+      // serif: ['Merriweather', 'serif'],
     },
     extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
+      // spacing: {
+      //   '128': '32rem',
+      //   '144': '36rem',
+      // },
+      // borderRadius: {
+      //   '4xl': '2rem',
+      // }
     },
   },
   plugins: [
